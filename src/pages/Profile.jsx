364e3319b87a573e9016d36e81fab4c63b99d33e
vikas <vickyvsvirat@gmail.com>
2023-10-02 18:01:33 +0530

    useEffect in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,31 @@
-import React, { useContext } from 'react'
-import { Context } from '../main';
+import React, { useContext, useEffect } from 'react'
+import { Context, server } from '../main';
 import Loader from '../components/Loader';
 import { Navigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Profile = () => {
-    const { isAuthenticated, loading, user } = useContext(Context);
+    const { isAuthenticated, loading, user, setLoading, setIsAuthenticated, setUser } = useContext(Context);
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`${server}/users/me`, {
+            withCredentials: true
+        }).then((res) => {
+            setUser(res.data.user);
+            setIsAuthenticated(true);
+            setLoading(false);
+        }
+        ).catch((err) => {
+            setUser({});
+            setIsAuthenticated(false);
+            setLoading(false);
+        })
+
+    }, []);
+
     if (!isAuthenticated) return <Navigate to={"/login"} />
+
     return (
         loading ? (<Loader />) :
             (<div className='profileContainer'>

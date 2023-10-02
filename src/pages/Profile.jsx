3cc1c16420fc5103318a38f07274f47cf7b6c78d
vikas <vickyvsvirat@gmail.com>
2023-10-02 17:08:54 +0530

    navigate

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Context } from '../main';
 import Loader from '../components/Loader';
+import { Navigate } from 'react-router-dom';
 
 const Profile = () => {
     const { isAuthenticated, loading, user } = useContext(Context);
